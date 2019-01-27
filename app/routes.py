4ada8a52850ef698e831e4e5160565b49d4272d6
Kyle Gimple <kylegimple@gmail.com>
2019-01-27 03:15:00 -0500

    Add files via upload
    CSS error fixed

--- app/routes.py
+++ app/routes.py
@@ -2,5 +2,10 @@ from flask import render_template
 from app import app
 @app.route('/')
 @app.route('/index')
+@app.route('/index_test')
 def index():
 	return render_template('index_test.html')
+
+@app.route('/TruckTest')
+def TruckTest():
+	return render_template('TruckTest.html')
